c83157de666edbdffbc992c4649994ec2dab7bfd
Navneet Dhar <navneetdhardubey.india@gmail.com>
2025-08-07 00:12:51 +0530

    Feat: Configure PostgreSQL database

--- socialmediaapi/settings.py
+++ socialmediaapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,10 +88,11 @@ WSGI_APPLICATION = 'socialmediaapi.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # This will use the DATABASE_URL from Render's environment variables
+        conn_max_age=600,
+        ssl_require=True, # Required for secure connection
+    )
 }
 
 
